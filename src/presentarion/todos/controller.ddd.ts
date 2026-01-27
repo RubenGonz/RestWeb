33f81614843dea24ed9a4f872152882b3aec2f4c
RubenGonz <ruben.gonzalez.rodriguez00@gmail.com>
2026-01-27 07:08:37 +0000

    Sección 16 - Rest - Clean Architecture

---
+++ src/presentarion/todos/controller.ddd.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres/init";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
+import { TodoRepository } from "../../domain";
+
+export class TodosController {
+
+  constructor(
+    private readonly todoRepository: TodoRepository
+  ) { }
+
+  public getTodos = async (req: Request, res: Response) => {
+    const todos = await this.todoRepository.getAll()
+    return res.json(todos)
+  }
+
+  public getTodoById = async (req: Request, res: Response) => {
+    const id = +req.params.id
+
+    try {
+      const todo = await this.todoRepository.findById(id)
+      res.json(todo)
+    } catch (error) {
+      res.status(404).json({ error })
+    }
+  }
+
+  public createTodo = async (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body)
+    if (error) return res.status(400).json({ error });
+
+    const todo = await this.todoRepository.create(createTodoDto!)
+    return res.json(todo)
+  }
+
+  public updateTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id
+    const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id })
+    if (error) res.status(400).json({ error: error })
+
+    const updatedTodo = await this.todoRepository.updateById(updateTodoDto!)
+    res.json(updatedTodo)
+  }
+
+  public deleteTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id
+    if (isNaN(id)) res.status(400).json({ error: `ID argument is not aa number` })
+
+    const deletedTodo = await this.todoRepository.deleteById(id)
+    return res.json(deletedTodo)
+  }
+}
